b49bbbd6cc2b294efc7be51eebd2c7622cc57016
amipatil <iamitpatil1993@gmail.com>
2020-04-08 22:17:13 +0530

    Initial Commit With required maven dependencies

---
+++ src/main/java/org/code4cause/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package org.code4cause.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/code4cause/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.code4cause.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
